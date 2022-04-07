ba022dbbcd82aac0c4e6086a3cd4227d24ae7a4b
TW - IT - Ray Lin <ray.lin@shoalter.com>
2022-04-07 17:13:00 +0800

    init

---
+++ src/main/java/com/example/grpcchatserver/GrpcChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcChatServerApplication.class, args);
+	}
+
+}
