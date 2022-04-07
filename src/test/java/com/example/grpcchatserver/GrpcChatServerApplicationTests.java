94f8a88951cab4926b00ae1d60fb6bcd3599396c
TW - IT - Ray Lin <ray.lin@shoalter.com>
2022-04-07 17:14:27 +0800

    feat: Empty test Class

---
+++ src/test/java/com/example/grpcchatserver/GrpcChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
