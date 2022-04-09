9e88d83385f9d0876a62994b5b37d209f76012ce
Ray Lin <mirror46258@gmail.com>
2022-04-09 08:34:04 +0800

    feat: Redis config

---
+++ src/main/java/com/example/grpcchatserver/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.example.grpcchatserver.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisTemplate) {
+        CacheManager cacheManager = RedisCacheManager.create(redisTemplate);
+        return cacheManager;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
